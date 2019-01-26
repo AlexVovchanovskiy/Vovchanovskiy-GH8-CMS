5e602dc9fde15c18825171659269854af41cd6d1
Alexandr Vovchanovskiy <al.vovchanovskiy@gmail.com>
2019-01-27 01:54:31 +0200

    Home15

--- Home15/index.php
+++ Home15/index.php
@@ -6,8 +6,31 @@
     <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-<header>
-</header>
+<?php
+
+$arr = array(3, 5, 1, 8, 7, -1, 4, 9, 0, 6, -3, 2);
+
+function sortInsert($arr)
+{
+    for ($i = 1; $i < count($arr); $i++) {
+        $key_value = $arr[$i];
+        $j = $i - 1;
+        while ($arr[$j] > $key_value) {
+            $arr[$j + 1] = $arr[$j];
+            $j = $j - 1;
+        }
+        $arr[$j + 1] = $key_value;
+    }
+    return $arr;
+}
+
+echo '<pre>';
+echo '<strong>Before sort:</strong>';
+print_r($arr);
+echo '<strong>After sort:</strong>';
+print_r(sortInsert($arr));
+echo '</pre>';
+?>
 
 <script src="assets/js/libs.js"></script>
 <script src="assets/js/main.js"></script>
